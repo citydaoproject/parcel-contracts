e9b279e064741fe002715251b330c324cf89ff37
Michael D. Norman <michael.norman@denormans.com>
2022-04-21 22:53:34 -0500

    chore: [#1] added pausable and tokenURI support

--- src/constants/roles.ts
+++ src/constants/roles.ts
@@ -7,6 +7,6 @@ export type AccessRole = string;
 export const toAccessRole = (value: BytesLike | Hexable | number): AccessRole => toByte32String(value);
 
 export const SUPER_ADMIN_ROLE = toAccessRole(0);
-export const PARCEL_MANAGER = keccak256(toUtf8Bytes('citydao.ParcelManager'));
+export const PARCEL_MANAGER_ROLE = keccak256(toUtf8Bytes('citydao.ParcelManager'));
 export const PAUSER_ROLE = keccak256(toUtf8Bytes('citydao.Pauser'));
 export const UPGRADER_ROLE = keccak256(toUtf8Bytes('citydao.Upgrader'));
--- test/setup/ChaiSetup.ts
+++ test/setup/ChaiSetup.ts
@@ -1,6 +1,4 @@
 import chai from 'chai';
 import { solidity } from 'ethereum-waffle';
-import chaiAsPromised from 'chai-as-promised';
 
 chai.use(solidity);
-chai.use(chaiAsPromised);
