31638987ad97dff4345812cabd61c795567d3a71
slyRacoon23 <earl.potters@gmail.com>
2022-04-25 11:09:43 -0600

    test[#1]: Initial test case for Nuclear Nerds

---
+++ test/contracts/NuclearNerds/NuclearNerds.test.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import { Signer } from "ethers";
+import { ethers, waffle } from "hardhat";
+
+
+describe("NuclearNerds", () => {
+
+    /* Local Variables */
+
+
+    before('setup signers and Deploy', async () => {
+        /* Setup */
+
+
+    });
+
+
+    describe("whitelist mint", async () => {
+
+
+
+    });
+
+    
+
+
+
+});
