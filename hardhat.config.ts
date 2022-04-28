d9f5ef0114b64a87a99dc944ac8127daded3e966
slyRacoon23 <earl.potters@gmail.com>
2022-04-28 13:17:29 -0600

    chore[#1]: Add hardhat configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config'; // https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
+import "@nomiclabs/hardhat-etherscan";
 import { HardhatUserConfig } from 'hardhat/config';
+
 
 const hardhatConfig: HardhatUserConfig = {
   solidity: {
@@ -19,6 +22,33 @@ const hardhatConfig: HardhatUserConfig = {
     outDir: 'types/contracts',
     target: 'ethers-v5',
   },
+
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_NODE_URI,
+      accounts: [process.env.WALLET_PRIVATE_KEY!], // https://bobbyhadz.com/blog/typescript-type-undefined-is-not-assignable-to-type-string
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+
+
+
 };
 
 export default hardhatConfig;
